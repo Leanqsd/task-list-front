d125fe15145b326ffa4bcf916482bc7134c548cc
Leanqsd <leandro.quesada.984@gmail.com>
2024-11-15 17:18:11 -0300

    creacion de loginview y registerview

---
+++ src/views/LoginView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import SignInComponent from '@/components/SignInComponent.vue'
+import RegisterComponent from '@/components/RegisterComponent.vue'
+</script>
+
+<template>
+  <SignInComponent />
+  <RegisterComponent />
+</template>
+
+<style></style>
